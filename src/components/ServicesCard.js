db8d93104fffcbb319c5a57ea6d9e337dc873c28
levaniakobidze <90929516+levaniakobidze@users.noreply.github.com>
2022-03-19 20:56:54 +0400

    #5 services component completed

--- src/components/ServicesCard.js
+++ src/components/ServicesCard.js
@@ -4,9 +4,9 @@ import './ServicesCard.css'
 function ServicesCard(props) {
   return (
     <div className='services-card'>
-            <div className="services-card__title">{props.title}</div>
+            <div className="services-card__title"><h3>{props.title}</h3></div>
             <div className="services-card__underline"></div>
-            <div className="services-card__text">{props.text}</div>
+            <div className="services-card__text"><p>{props.text}</p></div>
     </div>
   )
 }
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,11 +1,12 @@
 import React from "react";
-import Container from "../UI/Container";
+import Container from '../UI/Container'
+import ServicesCard from '../components/ServicesCard'
 import "./Services.css";
 function Services() {
   return (
     <div className="services">
-      <Container className="services-cont">
-        <div className="services-text-cont">
+    <Container className='services-cont'>
+    <div className="services-text-cont">
           <div className="services-underline"></div>
           <div className="services-header">
             <h1>SERVICES</h1>
@@ -13,17 +14,31 @@ function Services() {
           <div className="services-title">
             <p>Travel and change of place impart new vigor to the mind.</p>
           </div>
-
+          </div>
           <div className="services-card-cont">
-            <Services
+          <ServicesCard
+              title={"Hotel Bookings"}
+              text={
+                "Lorem ipsum dolor sit amet, id altera persequeris vim, mea an appareat prodesset. Cu mea error eripuit delicata. Nec soluta aliquid similique eu."
+              }
+            />
+
+<ServicesCard
+              title={"Hotel Bookings"}
+              text={
+                "Lorem ipsum dolor sit amet, id altera persequeris vim, mea an appareat prodesset. Cu mea error eripuit delicata. Nec soluta aliquid similique eu."
+              }
+            />
+
+<ServicesCard
               title={"Hotel Bookings"}
               text={
                 "Lorem ipsum dolor sit amet, id altera persequeris vim, mea an appareat prodesset. Cu mea error eripuit delicata. Nec soluta aliquid similique eu."
               }
             />
           </div>
-        </div>
-      </Container>
+        
+    </Container>
     </div>
   );
 }
